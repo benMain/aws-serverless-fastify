0611c04acf491ae9a75b5a3ad7fd576b481c5550
Benjamin Main <bmain@lumeris.com>
2019-07-02 14:48:24 -0500

    Fix index.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,8 +2,8 @@ import { Server, createServer } from 'http';
 import * as fastify from 'fastify';
 import { proxy } from './index';
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import {tmpdir} from 'os';
-import {join} from 'path';
+import { tmpdir } from 'os';
+import { join } from 'path';
 import { SocketManager } from './socket-manager';
 
 describe('RequestForwarder', () => {
@@ -22,7 +22,7 @@ describe('RequestForwarder', () => {
     const random = SocketManager.getSocketSuffix();
     const sockFile = join(tmpdir(), `${random}server.sock`);
     instance.get('/hello', async (request, reply) => {
-        return exampleResponse;
+      return exampleResponse;
     });
     await instance.listen(sockFile);
     const response = await proxy(server, event, context);
@@ -37,24 +37,24 @@ function buildGetEvent(): APIGatewayProxyEvent {
   return {
     body: null,
     headers: {
-        'Accept': 'application/json',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Cache-Control': 'max-age=0',
-        'CloudFront-Forwarded-Proto': 'https',
-        'CloudFront-Is-Desktop-Viewer': 'true',
-        'CloudFront-Is-Mobile-Viewer': 'false',
-        'CloudFront-Is-SmartTV-Viewer': 'false',
-        'CloudFront-Is-Tablet-Viewer': 'false',
-        'CloudFront-Viewer-Country': 'US',
-        'Host': '1234567890.execute-api.us-east-1.amazonaws.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Custom User Agent String',
-        'Via': '1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)',
-        'X-Amz-Cf-Id': 'cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==',
-        'X-Forwarded-For': '127.0.0.1, 127.0.0.2',
-        'X-Forwarded-Port': '443',
-        'X-Forwarded-Proto': 'http',
+      'Accept': 'application/json',
+      'Accept-Encoding': 'gzip, deflate, sdch',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Cache-Control': 'max-age=0',
+      'CloudFront-Forwarded-Proto': 'https',
+      'CloudFront-Is-Desktop-Viewer': 'true',
+      'CloudFront-Is-Mobile-Viewer': 'false',
+      'CloudFront-Is-SmartTV-Viewer': 'false',
+      'CloudFront-Is-Tablet-Viewer': 'false',
+      'CloudFront-Viewer-Country': 'US',
+      'Host': '1234567890.execute-api.us-east-1.amazonaws.com',
+      'Upgrade-Insecure-Requests': '1',
+      'User-Agent': 'Custom User Agent String',
+      'Via': '1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)',
+      'X-Amz-Cf-Id': 'cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==',
+      'X-Forwarded-For': '127.0.0.1, 127.0.0.2',
+      'X-Forwarded-Port': '443',
+      'X-Forwarded-Proto': 'http',
     },
     multiValueHeaders: {},
     httpMethod: 'GET',
