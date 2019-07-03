bc8e2a4833750287192216c763984d8547209605
Benjamin Main <bmain@lumeris.com>
2019-07-03 12:48:30 -0500

    refactor: Working on semantic-release
    Stop shipping dist back to github
    
    Please stop

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { Server } from 'http';
-import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
-export declare function proxy(server: Server, event: APIGatewayProxyEvent, context: Context, binaryTypes?: string[]): Promise<APIGatewayProxyResult>;
--- dist/request-forwarder.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { Server } from 'http';
-import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
-export declare class RequestForwarder {
-    static forwardRequestToNodeServer(server: Server, event: APIGatewayProxyEvent, context: Context, resolver: {
-        succeed: (data: APIGatewayProxyResult) => void;
-    }, binaryTypes: string[]): void;
-}
